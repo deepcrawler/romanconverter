05129f20e213351bf3638d61f424c8524dcef7de
h.ghaderipour <h.ghaderipour@DESKTOP-THU49D2>
2019-06-14 16:28:53 +0430

    update roman converter with adding error-handling scenarios
    DESC:
    Romanize function can only convert an integer bigger than zero. So, for
          - negative, zero, and non-absolute numbers,
          - non number values,
          - empty values (undefined, empty, null, empty string)
          - false boolean
    it returns an empty string("").

--- app/roman.js
+++ app/roman.js
@@ -1,16 +1,32 @@
 class Roman{
     romanize (num) {
-        var digits = String(+num).split(""),
-            key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-                   "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-                   "","I","II","III","IV","V","VI","VII","VIII","IX"],
-            roman = "",
-            i = 3;
-        while (i--){
-            roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-        }
+        const failedVal = "";
+        try{
+            if (isNaN(num) || 
+                num == null || 
+                num =="" || 
+                num < 0 || 
+                num % 1 !== 0 // Float numbers
+                ){
+                return failedVal;
+            }
 
-        return Array(+digits.join("") + 1).join("M") + roman;
+            var digits = String(+num).split(""),
+                key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                    "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                    "","I","II","III","IV","V","VI","VII","VIII","IX"],
+                roman = "",
+                i = 3;
+            while (i--){
+                roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+            }
+
+            let res = Array(+digits.join("") + 1).join("M") + roman;
+            return res === "" ? failedVal : res;
+        }
+        catch(exp){
+            return failedVal;
+        }
     }
     
 }
--- test/romanTest.js
+++ test/romanTest.js
@@ -3,6 +3,7 @@ const Roman     = require('../app/roman.js');
 
 describe("Roman Parser", function() {
     const roman=new Roman();
+    const failedValue="";
     
     describe("Int to Roman Converter", function() {
         it("convert int to roman", function() {
@@ -11,6 +12,66 @@ describe("Roman Parser", function() {
 
             res = roman.romanize("36");
             expect(res).to.equal("XXXVI");
+        });
+
+        it("convert string to roman", function() {
+            let res = roman.romanize("as");
+            expect(res).to.equal(failedValue);
+
+            
+            res = roman.romanize("");
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize("XXXX");
+            expect(res).to.equal(failedValue);
+        });
+
+        it("convert negative-zero int to roman", function() {
+            let res = roman.romanize("0");
+            expect(res).to.equal(failedValue);
+
+            
+            res = roman.romanize(0);
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize(-1);
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize(false); //0
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize(true); //1
+            expect(res).to.equal("I");
+        });
+
+        it("convert non-absoloute int to roman", function() {
+            let res = roman.romanize("0.1");
+            expect(res).to.equal(failedValue);
+
+            
+            res = roman.romanize(1.0);
+            expect(res).to.equal("I");
+
+            res = roman.romanize(1.1);
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize("1.0"); //1
+            expect(res).to.equal("I");
+        });
+
+        it("convert empty values to roman", function() {
+            let res = roman.romanize("");
+            expect(res).to.equal(failedValue);
+
+            
+            res = roman.romanize(null);
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize();
+            expect(res).to.equal(failedValue);
+
+            res = roman.romanize(undefined);
+            expect(res).to.equal(failedValue);
         });
     });
 });
